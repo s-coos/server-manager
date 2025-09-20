6612f231334d2c76376c35779cba35198c418852
Juyeong Maing <mjy9088@naver.com>
2025-09-21 02:27:08 +0900

    fix broken test

--- test/src-snapshots/02-restore/index.ts
+++ test/src-snapshots/02-restore/index.ts
@@ -14,6 +14,30 @@ app.get("/log/:message", (req, res) => {
   res.sendStatus(200);
 });
 
+app.post("/server-manager/prepare-to-be-active", (_req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/server-manager/prepare-to-be-non-active", (_req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/server-manager/activated", (_req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/server-manager/deactivated", (_req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/server-manager/cancel-prepare-to-be-active", (_req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/server-manager/cancel-prepare-to-be-non-active", (_req, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log("started");
 });
